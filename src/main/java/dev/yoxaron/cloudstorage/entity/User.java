e4fb391681b4dced6e263dfb016b2ac5a9394477
Dmitry Fadeev <fadeevdmitry97@gmail.com>
2026-03-22 02:43:51 +0000

    feat: add entity User and UserRepository

---
+++ src/main/java/dev/yoxaron/cloudstorage/entity/User.java
@@ -0,0 +1,22 @@
+package dev.yoxaron.cloudstorage.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+}
---
+++ src/main/java/dev/yoxaron/cloudstorage/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.yoxaron.cloudstorage.repository;
+
+import dev.yoxaron.cloudstorage.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
